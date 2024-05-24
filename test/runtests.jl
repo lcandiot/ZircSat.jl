3fed82bca73306fa1bf35dcfc2166f6aacc84431
Lorenzo Candioti <lorenzo.candioti@bluewin.ch>
2024-05-24 21:06:01 +0200

    Added maximum temperature change as convergence criterion.

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,6 +9,6 @@ header    = true
 T_0       = 1092.0
 
 @testset "ZircSat.jl" begin
-    @test calculate_liquidus_temperature(; T_0, data_path, db, sys_in, delim, header) ≈ [1093.6] rtol = 1e-1
+    @test calculate_liquidus_temperature(; T_0, data_path, db, sys_in, delim, header) ≈ [1094.1] rtol = 1e-1
     @test calculate_ZircSat_temperature(; data_path) ≈ [698.7] rtol = 1e-1
 end
